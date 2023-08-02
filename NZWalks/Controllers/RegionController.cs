732ce68a70e854529139fb211fb76fa4b26d3f33
Vasu Malhotra <vasu4real@Vasus-MacBook-Pro.local>
2023-08-02 15:47:41 +0530

    Async

--- NZWalks/Controllers/RegionController.cs
+++ NZWalks/Controllers/RegionController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.AspNetCore.Mvc;
 using NZWalks.Data;
 using NZWalks.Model.Domain;
@@ -22,10 +23,10 @@ namespace NZWalks.Controllers
             this.dBContext = dBContext;
         }
         [HttpGet]
-        public IActionResult GetAll()
+        public async Task<IActionResult> GetAll()
         {
             // get Data from DB to Domain Model
-            var regionsDomain = dBContext.Regions.ToList();
+            var regionsDomain = await dBContext.Regions.ToListAsync();
             // Map the data to DTO
 
             List<RegionDto> regionDto = new List<RegionDto>();
@@ -46,9 +47,9 @@ namespace NZWalks.Controllers
         }
         [HttpGet]
         [Route("{id:Guid}")]
-        public IActionResult GetById([FromRoute] Guid id)
+        public async Task<IActionResult> GetById([FromRoute] Guid id)
         {
-            var region = dBContext.Regions.Find(id);
+            var region = await dBContext.Regions.FindAsync(id);
 
             if (region == null)
             {
@@ -69,7 +70,7 @@ namespace NZWalks.Controllers
 
         }
         [HttpPost]
-        public IActionResult Create([FromBody] AddRegionRequestDto addRegionRequestDto)
+        public async Task<IActionResult> Create([FromBody] AddRegionRequestDto addRegionRequestDto)
         {
             //Map regioDTO to regionDomain
             Region regionDomainModel = new Region()
@@ -80,8 +81,8 @@ namespace NZWalks.Controllers
             };
             // Add a DB record using DB context
 
-            dBContext.Regions.Add(regionDomainModel);
-            dBContext.SaveChanges();
+            await dBContext.Regions.AddAsync(regionDomainModel);
+            await dBContext.SaveChangesAsync();
             //Map again to Region DTO to pass the values to Client
             var regionDto = new RegionDto()
             {
@@ -96,10 +97,10 @@ namespace NZWalks.Controllers
         }
         [HttpPut]
         [Route("{id:Guid}")]
-        public IActionResult Update([FromRoute] Guid id, [FromBody] UpdateRegionRequestDto updateRegionRequestDto)
+        public async Task<IActionResult> Update([FromRoute] Guid id, [FromBody] UpdateRegionRequestDto updateRegionRequestDto)
         {
             //check if the id exisit
-            var regionDomainModel = dBContext.Regions.FirstOrDefault(x => x.Id == id);
+            var regionDomainModel =  await dBContext.Regions.FirstOrDefaultAsync(x => x.Id == id);
             if(regionDomainModel == null)
             {
                 return NotFound();
@@ -125,15 +126,15 @@ namespace NZWalks.Controllers
 
         [HttpDelete]
         [Route("{id:Guid}")]
-        public IActionResult Delete([FromRoute]Guid id)
+        public async Task<IActionResult> Delete([FromRoute]Guid id)
         {
-            var regionDomainmodel = dBContext.Regions.FirstOrDefault(x => x.Id == id);
+            var regionDomainmodel = await dBContext.Regions.FirstOrDefaultAsync(x => x.Id == id);
             if (regionDomainmodel==null)
             {
                 return NotFound();
             }
             dBContext.Regions.Remove(regionDomainmodel);
-            dBContext.SaveChanges();
+            await dBContext.SaveChangesAsync();
             return Ok();
         }
     }
